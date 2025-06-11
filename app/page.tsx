fda1c4543baa03b6ffc41f261e296a8f1631b19e
Samuel Serrato <samysd12@hotmail.com>
2025-06-10 20:53:56 -0600

    prueba2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import { Hero } from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { Grid } from "@/components/Grid";
-import { RecentProjects } from "@/components/RecentProjects";
 import { navItems } from "@/data";
-import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
-import { Approach } from "@/components/Approach";
-import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
@@ -15,9 +9,6 @@ export default function Home() {
         <FloatingNav
           navItems={navItems}
         />
-        <Hero/>
-        <Grid/>
-        <Footer/>
       </div>
     </main>
   );
